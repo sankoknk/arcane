436ae046d21a36315ad1f6af8ea049043ca8873d
sankoknk <sam.sebe.krouly@gmail.com>
2024-11-30 21:18:21 +0300

    бебеббебебеббебебеббебебебеббебе

--- code1.py
+++ code1.py
@@ -24,8 +24,8 @@ plblock_speed = 8
 ball_r = 40
 ball = pygame.Rect(screen_w // 2 - ball_r // 2, screen_h - ball_r - plblock_h - 60, ball_r, ball_r)
 ball_speed = 4
-dir_st = 1  # НАПРАВЛЕНИЕ ДВИЖЕНИЯ
-dir_op = -1  # ИЗМЕНЕНИЕ НАПРАВЛЕНИЯ ДВИЖЕНИЯ
+dir_x = 1  # НАПРАВЛЕНИЕ ДВИЖЕНИЯ
+dir_y = -1  # ИЗМЕНЕНИЕ НАПРАВЛЕНИЯ ДВИЖЕНИЯ
 
 # ЗАДАЕМ МОМЕНТ ВЫКЛЮЧЕНИЯ ПРОГРАМЫ
 while True:
@@ -42,16 +42,16 @@ while True:
         plblock.centerx += plblock_speed
 
     # ЗАДАЕМ ДВИЖЕНИЕ ШАРИКА
-    ball.x += ball_speed * dir_st
-    ball.y += ball_speed * dir_op
+    ball.x += ball_speed * dir_x
+    ball.y += ball_speed * dir_y
     if screen_w - ball_r < ball.x:
-        dir_st = - dir_st
+        dir_x = - dir_x
     if screen_h - ball_r < ball.y:
-        dir_op = - dir_op
+        dir_y = - dir_y
 
     # ОПРЕДЕЛЯЕМ СТОЛКНОВЕНИЕ С ПЛАТФОРМОЙ
-    if ball.colliderect(plblock) and dir_op > 0:
-        dir_op = - dir_op
+    if ball.colliderect(plblock) and dir_y > 0:
+        dir_y = - dir_y
 
     # screen.blit(image, (0,0))
     screen.fill((0, 0, 0))
